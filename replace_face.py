d550dd52d97df12f12b414a32c08c6459c47fa29
daniel-earley <danielse08@gmail.com>
2022-01-22 12:57:46 -0500

    Initial Commit

--- replace_face.py
+++ replace_face.py
@@ -1,47 +1,49 @@
-import cv2
+import pygame as pg
+import sys
 import os
+import cv2
+from pygame.locals import *
+
+# Inits
+pg.init()
 
 # File Paths
 xmlName = 'haarcascade_frontalface_default.xml'
 resourcePath = os.path.dirname(__file__)
 resourcePath = os.path.join(resourcePath, 'resources')
 xmlPath = os.path.join(resourcePath, xmlName)
-replaceImg = os.path.join(resourcePath, 'Red Ranger Green Cropped.png')
-newImg = cv2.imread(replaceImg)
 
 # Need cascade classifier to read the haarcascade xml
 cascade = cv2.CascadeClassifier(xmlPath)
 
-# Capture Webcam
+# Setup pygame window and webcam
+W, H = 640, 480
+screen = pg.display.set_mode((W, H))
 webcam = cv2.VideoCapture(0)
 
 # Loop to capture video
 while True:
-    # Capture and convert frames to grayscale
-    unusedVar, img = webcam.read()
-    grayscaleFrame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = cascade.detectMultiScale(grayscaleFrame, 1.1, 15)
+    unusedVar, cvImg = webcam.read()
+    cv2.imwrite("./resources/frame.jpg", cvImg)
+    grayscale = cv2.cvtColor(cvImg, cv2.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(grayscale, 1.1, 15)
+    camImg = pg.image.load("./resources/frame.jpg")
+    newImg = pg.image.load("./resources/default.png")
+    screen.blit(camImg, (0,0))
 
-
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
-    newImg = cv2.cvtColor(newImg, cv2.COLOR_BGR2BGRA)
-
-    # Draw a rectangle over the detected face
+    # Draw a rectangle over detected face
     for (x,y,w,h) in faces:
-        newImg = cv2.resize(newImg, (w,h))
-        img[y:y+w, x:x+h] = newImg
-        # cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 2)
-        
+        newImg = pg.transform.scale(newImg, (w*2, h*2))
+        screen.blit(newImg, (x/2, y/2))
+
+    # Update Display
+    pg.display.update()
+
+    # check for exit
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+            webcam.release()
+            sys.exit()
     
 
-    # for i in range(0, w):
-    #     for j in range(0, h):
-    #         if newImg[i,j][2] != 0:
-    #             img[y+i, x+j] = newImg[i, j]
-    cv2.imshow('img', img)
-
-    key = cv2.waitKey(30) & 0xff
-    if key == ord(']'):
-        break
-
-webcam.release()
